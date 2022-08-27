7e95560c7d7dddb66846a20202b18150a5ce5432
José Marín <josemp366@ciencias.unam.mx>
2022-08-27 16:58:42 -0500

    working on it

---
+++ Practica2/MiPagina/js/script1.js
@@ -0,0 +1,20 @@
+//alert("Soy un mensaje de js");
+
+//let nombre = prompt("Ingresa tu nombre.");
+//document.write(nombre);
+
+let a = 3;
+let b = 'a';
+let c = "Hola soy una cadena";
+//document getElementById("var").innerHTML = document.write("Soy un numero entero: "+a+"<br> Soy un caracter: "+b+"<br> Soy una cadena: "+c);
+//document.write("Soy un numero entero: "+a+"<br> Soy un caracter: "+b+"<br> Soy una cadena: "+c);
+
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
+
+const miTitulo = document.querySelector('h2');
+miTitulo.textContent = '¡Hola mundo!';
+
+function hola(){
+    document.write("Holaaaa");
+}
