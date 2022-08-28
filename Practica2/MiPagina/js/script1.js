35afb105d096c7e4b3bae475262501b36902e96b
José Marín <josemp366@ciencias.unam.mx>
2022-08-28 14:26:51 -0500

    Practica2

--- Practica2/MiPagina/js/script1.js
+++ Practica2/MiPagina/js/script1.js
@@ -1,3 +1,4 @@
+//Función que usa variables
 function cadenas(){
     let a = 3;
     let b = 'a';
@@ -5,15 +6,56 @@ function cadenas(){
     document.write("Soy un numero entero: "+a+"<br> Soy un caracter: "+b+"<br> Soy una cadena: "+c);
 }
 
-
-function arregloCarro(){
+//Función que usa arreglos
+function arregloComponentes(){
     const componentes = ["CPU","GPU","Motherboard","RAM","Almacenamiento","Fuente de poder","Gabinete"];
     document.write(componentes);
 }
 
-function ciclo(){
-    const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-    for (let i = 0; i < cars.length; i++) {
-        document.write(cars[i] + "<br>");
+//Función que usa ciclo for
+function cicloFor(){
+    const pares = [2,4,6,8,10,12,14];
+    for (let i = 0; i < pares.length; i++) {
+        document.write("-> " + pares[i] + "<br>");
     }
+}
+
+//Función que usa ciclo while
+function cicloWhile(){
+    let i = 0;
+    while (i <= 10) {
+        document.write("Numero " + i + "<br>");
+        i++;
+    }
+    document.getElementById("demo").innerHTML = text;
+}
+
+//Función que usa la condicional if
+function condIf(){
+    var form1 = document.getElementById('formulario');
+    if(form1.v1.value > form1.v2.value){
+        alert(form1.v1.value + " es mayor que " + form1.v2.value);
+        return true;
+    }else if(form1.v2.value > form1.v1.value){
+        alert(form1.v2.value + " es mayor que " + form1.v1.value);
+        return true;
+    }else{
+        alert(form1.v1.value + " es igual que " + form1.v2.value);
+        return true;
+    }
+    return true;
+}
+
+//Función que atiende y valida datos de formulario
+function validarForm(){
+    let form = document.getElementById('valida');
+    if(form.usuario.value == ""){
+        alert("Ingresa un nombre de usuario");
+        return false;
+    }else if(form.contra.value != form.contraConf.value){
+        alert("Las contraseñas deben ser iguales");
+        return false;
+    }
+    alert("Hola " + form.usuario.value + " :D");
+    return true;
 }
